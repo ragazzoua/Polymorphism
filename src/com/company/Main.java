a6f15092ddc40e5a6d5cd9e39745f4d55893d448
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-09-04 14:38:53 +0300

    random Movie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,48 +16,48 @@ class Jaws extends Movie {
     public Jaws() {
         super("Jaws");
     }
+}
 
     public String plot() {
         return "A shark eats lots of people";
     }
 
-    class IndependenceDay extends Movie {
-        public IndependenceDay() {
-            super("Independence Day");
-        }
-
-        @Override
-        public String plot() {
-            return "Aliens attempt to take over planer earth";
-        }
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
     }
 
-    class MazeRunner extends Movie {
-        public MazeRunner() {
-            super("Maze Runner");
-        }
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planer earth";
+    }
+}
 
-        @Override
-        public String plot() {
-            return "Kids try and escape a maze";
-        }
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
     }
 
-    class StarWars extends Movie {
-        public StarWars() {
-            super("Star Wars");
-        }
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze";
+    }
+}
 
-        @Override
-        public String plot() {
-            return "Imperial Forces take to over the universe";
-        }
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
     }
 
-    class Forgetable extends Movie {
-        public Forgetable() {
-            super("Forgetable");
-        }
+    @Override
+    public String plot() {
+        return "Imperial Forces take to over the universe";
+    }
+}
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
     }
 }
 
@@ -65,6 +65,27 @@ class Jaws extends Movie {
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+    }
+
+    public static Movie randomMovie() {
+        int ranromNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was" + ranromNumber);
+        switch (ranromNumber) {
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+        return null;
     }
 }
